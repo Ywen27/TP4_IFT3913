9e91fd38bb40864a1e9261a0b9c596f6e36d935a
Wen Yin <andywenyin@gmail.com>
2023-11-22 19:02:07 -0500

    update test classe

--- currency-converter-in-java/src/test/CurrencyTest.java
+++ currency-converter-in-java/src/test/CurrencyTest.java
@@ -1,4 +1,59 @@
 package test;
 
+import currencyConverter.Currency;
+import currencyConverter.MainWindow;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CurrencyTest {
+    @Test
+    public void testValidAmountConversion() {
+        double amount = 50000.0; // Montant valide
+        double exchangeRate = 0.8; // Taux de change hypothétique
+        double convertedAmount = Currency.convert(amount, exchangeRate);
+        assertEquals(40000.0, convertedAmount);
+    }
+
+    @Test
+    public void testZeroAmount() {
+        double amount = 0; // Montant à la limite inférieure
+        double exchangeRate = 0.8;
+        double convertedAmount = Currency.convert(amount, exchangeRate);
+        assertEquals(0, convertedAmount);
+    }
+
+    @Test
+    public void testUpperBoundaryAmount() {
+        double amount = 1000000.0; // Montant à la limite supérieure
+        double exchangeRate = 0.8;
+        double convertedAmount = Currency.convert(amount, exchangeRate);
+        assertEquals(800000.0, convertedAmount);
+    }
+
+    // Hypothèse : le montant converti est -1 pour les entrées non valides
+    @Test
+    public void testNegativeAmount() {
+        double amount = -100.0; // Montant négatif
+        double exchangeRate = 0.8;
+        double convertedAmount = Currency.convert(amount, exchangeRate);
+        assertEquals(-1, convertedAmount);
+    }
+
+    @Test
+    public void testAmountOutOfRange() {
+        double amount = 1000001.0; // Montant supérieur à la limite
+        double exchangeRate = 0.8;
+        double convertedAmount = Currency.convert(amount, exchangeRate);
+        assertEquals(-1, convertedAmount);
+    }
+
+    @Test
+    public void testInvalidCurrencyExchange() {
+        double amount = 100.0;
+        double exchangeRate = -0.5; // Taux de change invalide
+        double convertedAmount = Currency.convert(amount, exchangeRate);
+        assertEquals(-1, convertedAmount);
+    }
+
+
 }
--- currency-converter-in-java/src/test/MainWindowTest.java
+++ currency-converter-in-java/src/test/MainWindowTest.java
@@ -10,30 +10,52 @@ import java.util.ArrayList;
 public class MainWindowTest {
     private ArrayList<Currency> currencies = Currency.init();
 
-    // Test de conversion valide entre USD et EUR
     @Test
-    public void testValidUSDtoEURConversion() {
-        double amount = 500.0; // Montant valide
-        double convertedAmount = MainWindow.convert("USD", "EUR", currencies, amount);
-        assertTrue(convertedAmount > 0);
+    public void testValidConversion() {
+        double amount = 100.0; // Montant de test
+        String[] Currencies = {"USD", "EUR", "GBP", "CHF", "CNY", "JPY"};
+        for (String fromCurrency : Currencies) {
+            for (String toCurrency : Currencies) {
+                double convertedAmount = MainWindow.convert(fromCurrency, toCurrency, currencies, amount);
+                assertTrue(convertedAmount >= 0);
+            }
+        }
     }
 
-    // Test pour un montant en dehors de la plage valide
+    @Test
+    public void testZeroAmount() {
+        double amount = 0.0;
+        double convertedAmount = MainWindow.convert("USD", "EUR", currencies, amount);
+        assertEquals(0, convertedAmount);
+    }
+
+    @Test
+    public void testUpperBoundaryAmount() {
+        double amount = 1000000.0;
+        double convertedAmount = MainWindow.convert("USD", "EUR", currencies, amount);
+        assertTrue(convertedAmount >= 0);
+    }
+
+    // Hypothèse : le montant converti est -1 pour les entrées non valides
     @Test
     public void testAmountOutOfRange() {
         double amount = 1000001.0; // Montant supérieur à la limite
         double convertedAmount = MainWindow.convert("USD", "EUR", currencies, amount);
-        assertEquals(0, convertedAmount); // Hypothèse : le montant converti est 0 pour les entrées non valides
+        assertEquals(-1, convertedAmount);
     }
 
     @Test
     public void testInvalidAmount() {
-        ArrayList<Currency> currencies = Currency.init();
         double amount = -100.0; // Montant invalide
         double convertedAmount = MainWindow.convert("USD", "EUR", currencies, amount);
+        assertEquals(-1, convertedAmount);
+    }
 
-        // Assert que le montant converti est 0 ou une valeur d'erreur
-        assertEquals(0, convertedAmount);
+    @Test
+    public void testConversionWithUnsupportedCurrency() {
+        double amount = 100.0;
+        double convertedAmount = MainWindow.convert("USD", "XYZ", currencies, amount);
+        assertEquals(-1, convertedAmount);
     }
 
 }
